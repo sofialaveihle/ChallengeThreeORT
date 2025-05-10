1570dc5a25aec67dfc34d3be0420b58532d5ddc5
Sofia Laveihle <sofialaveihle@gmail.com>
2025-05-10 19:34:47 -0300

    First commit

--- app/src/main/java/ar/edu/ort/challengethreeort/ui/theme/Theme.kt
+++ app/src/main/java/ar/edu/ort/challengethreeort/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package ar.edu.ort.challengethreeort.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
