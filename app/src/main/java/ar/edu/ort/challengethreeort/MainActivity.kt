f141779bdb6acd0558ff1772e63058f8bdc44395
Sofia Laveihle <sofialaveihle@gmail.com>
2025-05-11 00:36:49 -0300

    Creación de componentes y modificación de colores

--- app/src/main/java/ar/edu/ort/challengethreeort/MainActivity.kt
+++ app/src/main/java/ar/edu/ort/challengethreeort/MainActivity.kt
@@ -19,7 +19,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.VerticalAlignmentLine
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import ar.edu.ort.challengethreeort.components.QuoteCard
